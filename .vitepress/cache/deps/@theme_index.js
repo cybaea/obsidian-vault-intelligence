f0b9e2de3647490cd682f2188dafc366862783c5
Allan Engelhardt <Allan.Engelhardt@cybaea.net>
2026-01-24 15:02:18 +0000

    feat: Enhance documentation site with Mermaid support, local search, GitHub integration, sitemap generation, and automated Open Graph images.

--- .vitepress/cache/deps/@theme_index.js
+++ .vitepress/cache/deps/@theme_index.js
@@ -7,6 +7,7 @@ import {
   shallowRef,
   watch
 } from "./chunk-QE257C5J.js";
+import "./chunk-BUSYA2B4.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
 import "/home/allane/Code/GitHub/obsidian-vault-intelligence/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
